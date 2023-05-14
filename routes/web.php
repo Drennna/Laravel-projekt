5aa8bb40d230e443bc211a3bafdef9e47c3a768e
Drennna <agata.krol2@student.wsb.edu.pl>
2023-05-14 09:44:34 +0200

    add people CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PeopleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/people', [PeopleController::class, 'getAll'] );
+Route::post('/people', [PeopleController::class, 'createOne'] );
+Route::put('/people/{id}', [PeopleController::class, 'updateOne'] );
+Route::get('/people/{id}', [PeopleController::class, 'getOne'] );
+Route::delete('/people/{id}', [PeopleController::class, 'deleteOne'] );
